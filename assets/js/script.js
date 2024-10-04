558434bc853edeca56739501538b59ff06e298e8
Utsav Bhesnaiya <ukbhesaniya01@gmail.com>
2024-10-04 23:02:05 +0530

    All Vaidation Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,114 +8,25 @@ let cpass = document.getElementById("cpass");
 const submitForm = () =>{
     event.preventDefault();
 
-    const emailPattern = /^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$/gm;
+    nameFun();
+    emailFun();
+    phoneFun();
+    passFun();
+    cpassFun();
 
-    const phonePattern = /^(\+\d{1,3}[- ]?)?\d{10}$/;
+    const nameErr = document.getElementById("nameErr").innerHTML;
+    const emailErr = document.getElementById("emailErr").innerHTML;
+    const conErr = document.getElementById("conErr").innerHTML;
+    const passErr = document.getElementById("passErr").innerHTML;
+    const cpassErr = document.getElementById("cpassErr").innerHTML;
+    
+    if(nameErr == '' && emailErr == '' && conErr == '' && passErr == '' && cpassErr == ''){
 
-    const passPattern = /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{6,})\S$/;
-
-    nameVal = name.value;
-    emailVal = email.value;
-    phoneVal = phone.value;
-    passVal = pass.value;
-    cpassVal = cpass.value; 
-
-    // Name Validation
-    if(nameVal.trim() == ''){
-
-        document.getElementById("nameErr").innerHTML = "Name Must Be Required";
-
-        name.style.borderColor = "red";
-    }else{
-     
-        if(nameVal.trim().length < 3){
-
-            name.style.borderColor = "red";
-            document.getElementById("nameErr").innerHTML = "Name contains at least 3 charaters";
-        }else{
-
-            document.getElementById("nameErr").innerHTML = "";
-            name.style.borderColor = "white";
-        }
-    }
-
-    // Email Validation
-    if(emailVal.trim() == ''){
-
-        document.getElementById("emailErr").innerHTML = "Email Must Be Required";
-
-        email.style.borderColor = "red";
+        alert("Form Submitted Succesfully..");
     }else{
 
-        if(!emailVal.match(emailPattern)){
-
-            email.style.borderColor = "red";
-            document.getElementById("emailErr").innerHTML = "Email Must Be In Proper Format";
-        }else{
-
-            document.getElementById("emailErr").innerHTML = "";
-            email.style.borderColor = "white";
-        }
-    }
-
-    // Phone Number Validation
-    if(phoneVal.trim() == ''){
-
-        document.getElementById("conErr").innerHTML = "Phone Number Must Be Required";
-
-        phone.style.borderColor = "red";
-    }else{
-
-        if(!phoneVal.match(phonePattern)){
-
-            phone.style.borderColor = "red";
-            document.getElementById("conErr").innerHTML = "Phone Number Must Be Only 10 Digits";
-        }else{
-
-            document.getElementById("conErr").innerHTML = "";
-            phone.style.borderColor = "white";
-        }
-    }
-
-    // Password Validation
-    if(passVal.trim() == ''){
-
-        document.getElementById("passErr").innerHTML = "Password Must Be Required";
-
-        pass.style.borderColor = "red";
-    }else{
-
-        if(!passVal.match(passPattern)){
-
-            pass.style.borderColor = "red";
-            document.getElementById("passErr").innerHTML = "Password Contains Must One uppercase character, one number, And at least 8 characters";
-        }else{
-
-            document.getElementById("passErr").innerHTML = "";
-            pass.style.borderColor = "white";
-        }
-    }
-
-    // Confirm Password Validation
-    if(cpassVal.trim() == ''){
-
-        document.getElementById("cpassErr").innerHTML = "Confirm Password Must Be Required";
-
-        cpass.style.borderColor = "red";
-    }else{
-
-        if(cpassVal.length != passVal.length){
-            
-            cpass.style.borderColor = "red";
-
-            document.getElementById("cpassErr").innerHTML = "Password & Confirm Password Must Be The Same";
-        }else{
-
-            document.getElementById("cpassErr").innerHTML = "";
-            cpass.style.borderColor = "white";
-        }
-    }
-
+        alert("Please Fill Correct Information In Form..!!!");
+    }    
 }
 
 
@@ -130,16 +41,135 @@ const nameFun = () => {
         document.getElementById("nameErr").innerHTML = "Name Must Be Required";
 
         name.style.borderColor = "red";
+        document.querySelector(".namecolor").style.color = "red";
     }else{
      
         if(nameVal.trim().length < 3){
 
             name.style.borderColor = "red";
             document.getElementById("nameErr").innerHTML = "Name contains at least 3 charaters";
+            document.querySelector(".namecolor").style.color = "red";
         }else{
 
             document.getElementById("nameErr").innerHTML = "";
             name.style.borderColor = "white";
+            document.querySelector(".namecolor").style.color = "white";
+        }
+    }
+}
+
+// Email Validation
+const emailFun = () => {
+
+    const emailPattern = /^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$/gm;
+
+    emailVal = email.value;
+
+    if(emailVal.trim() == ''){
+
+        document.getElementById("emailErr").innerHTML = "Email Must Be Required";
+
+        email.style.borderColor = "red";
+        document.querySelector(".emailcolor").style.color = "red";
+    }else{
+
+        if(!emailVal.match(emailPattern)){
+
+            email.style.borderColor = "red";
+            document.getElementById("emailErr").innerHTML = "Email Must Be In Proper Format";
+            document.querySelector(".emailcolor").style.color = "red";
+        }else{
+
+            document.getElementById("emailErr").innerHTML = "";
+            email.style.borderColor = "white";
+            document.querySelector(".emailcolor").style.color = "white";
+        }
+    }
+}
+
+// Phone Number Validation
+const phoneFun = () => {
+
+    const phonePattern = /^\d{10}$/;
+    phoneVal = phone.value;
+
+    if(phoneVal.trim() == ''){
+
+        document.getElementById("conErr").innerHTML = "Phone Number Must Be Required";
+
+        phone.style.borderColor = "red";
+        document.querySelector(".concolor").style.color = "red";
+    }else{
+
+        if(!phoneVal.match(phonePattern)){
+
+            phone.style.borderColor = "red";
+            document.getElementById("conErr").innerHTML = "Phone Number Must Be Only 10 Digits";
+            document.querySelector(".concolor").style.color = "red";
+        }else{
+
+            document.getElementById("conErr").innerHTML = "";
+            phone.style.borderColor = "white";
+            document.querySelector(".concolor").style.color = "white";
+        }
+    }
+}
+
+// Password Validation
+const passFun = () => {
+
+    const passPattern = /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,16}$/;
+    passVal = pass.value; 
+
+    if(passVal.trim() == ''){
+
+        document.getElementById("passErr").innerHTML = "Password Must Be Required";
+
+        pass.style.borderColor = "red";
+        document.querySelector(".passcolor").style.color = "red";
+    }else{
+
+        if(!passVal.match(passPattern)){
+
+            pass.style.borderColor = "red";
+            document.getElementById("passErr").innerHTML = "Password Contains Must One uppercase character, one number, One Non-Alpha numeric Character And at least 8 characters";
+            document.querySelector(".passcolor").style.color = "red";
+        }else{
+
+            document.getElementById("passErr").innerHTML = "";
+            pass.style.borderColor = "white";
+            document.querySelector(".passcolor").style.color = "white";
+        }
+    }
+
+    
+    
+}
+
+// Confirm Password Validation
+const cpassFun = () => {
+
+    cpassVal = cpass.value;
+
+    if(cpassVal.trim() == ''){
+
+        document.getElementById("cpassErr").innerHTML = "Confirm Password Must Be Required";
+
+        cpass.style.borderColor = "red";
+        document.querySelector(".cpasscolor").style.color = "red";
+    }else{
+
+        if(cpassVal.length != passVal.length){
+            
+            cpass.style.borderColor = "red";
+
+            document.getElementById("cpassErr").innerHTML = "Password & Confirm Password Must Be The Same";
+            document.querySelector(".cpasscolor").style.color = "red";
+        }else{
+
+            document.getElementById("cpassErr").innerHTML = "";
+            cpass.style.borderColor = "white";
+            document.querySelector(".cpasscolor").style.color = "white";
         }
     }
 }
